78e6870813f230ab2d3aa73314601a2866d0563d
Zachary Jayes <zacjayes@gmail.com>
2021-01-23 12:08:04 -0600

    can receive post body

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded( { extended: true } ))
+app.use(express.json())
+
 const characters = [
     {
         name: 'Yoda',
@@ -33,6 +36,11 @@ app.get('/', (req, res) => {
     res.send('May the force be with you!')
 })
 
+/**
+ * API ROUTES
+ */
+
+
 
 // /api/characters - show all character data
 app.get('/api/characters', (req, res)  => {
@@ -51,6 +59,12 @@ app.get('/api/characters/:routeName', (req, res) => {
     })
 
    res.json(character)
+})
+
+
+app.post('/api/characters/add', (req, res) => {
+    console.log(req.body)
+    res.end()
 })
 
 app.listen(PORT, () =>{
