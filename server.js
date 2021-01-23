25e04a5ebb39efa25acabe8879538cc63a60fddb
Zachary Jayes <zacjayes@gmail.com>
2021-01-23 13:00:37 -0600

    api done

--- server.js
+++ server.js
@@ -63,8 +63,11 @@ app.get('/api/characters/:routeName', (req, res) => {
 
 
 app.post('/api/characters/add', (req, res) => {
-    console.log(req.body)
-    res.end()
+    const newCharacter = req.body
+    newCharacter.routeName = newCharacter.name.replace(/ /g, '').toLowerCase()
+    characters.push(newCharacter)
+    console.log(characters)
+    res.status(200).send()
 })
 
 app.listen(PORT, () =>{
